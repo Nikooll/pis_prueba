404e52dc1ee468f50eb961e377d25edb3d4e5fad
Nikooll Camacho <166524335+Nikooll@users.noreply.github.com>
2024-07-09 09:17:02 -0500

    Delete ProyectoIntegradorPIS/ProyectoIntegradorPIS directory

--- ProyectoIntegradorPIS/ProyectoIntegradorPIS/ProyectoIntegradorSaberes/composicionBrazo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from composicionBrazo.models import Base, Forearm, Gripper
-
-# Register your models here.
-
-admin.site.register(Base)
-admin.site.register(Forearm)
-admin.site.register(Gripper)
--- ProyectoIntegradorPIS/ProyectoIntegradorPIS/ProyectoIntegradorSaberes/movimientoBrazo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from movimientoBrazo.models import StepperMotor, ServoMotor, Encoder, Boton
-
-# Register your models here.
-admin.site.register(StepperMotor)
-admin.site.register(ServoMotor)
-admin.site.register(Encoder)
-admin.site.register(Boton)
